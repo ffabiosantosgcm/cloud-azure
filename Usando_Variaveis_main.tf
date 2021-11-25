e0948f21f495084ef73d348f90fab9f915d9f88d
Fábio Santos Gomes <73297075+ffabiosantosgcm@users.noreply.github.com>
2021-11-25 16:33:52 -0300

    Adicionando a opção Public IP

--- Usando_Variaveis_main.tf
+++ Usando_Variaveis_main.tf
@@ -28,6 +28,18 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Create public IPs
+resource "azurerm_public_ip" "puc-public-ip" {
+  name                = "${var.prefix}-public"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.grupo-recurso.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "PosGraduacao"
+  }
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = var.location
@@ -37,6 +49,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.puc-public-ip.id
   }
 }
 
@@ -45,12 +58,12 @@ resource "azurerm_virtual_machine" "main" {
   location              = var.location
   resource_group_name   = azurerm_resource_group.grupo-recurso.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_B1s"
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
   storage_os_disk {
